660c878cb43770603f33cd808c2ba4399a66f548
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2022-03-11 17:33:58 -0300

    final commit

--- src/main/java/br/com/fiap/testes/TestesCentroColeta.java
+++ src/main/java/br/com/fiap/testes/TestesCentroColeta.java
@@ -23,11 +23,11 @@ public class TestesCentroColeta {
 //
 //				// Criando objetos CentroColeta
 //		
-//				CentroColeta centroColeta1 = new CentroColeta(0,"Hemocentro Unifesp"," xx.xxx.xxx/xxxx-xx", "Rua Botucatu, 620","04023-062","(11) 5539-7289", null);
-//				CentroColeta centroColeta2 = new CentroColeta(0,"Banco de Sangue Paulista"," xx.xxx.xxx/xxxx-xx", " Rua Iguatinga, 396","04744-040","(11) 5681-6222", null);
-//				CentroColeta centroColeta3 = new CentroColeta(0,"Hospital Cruz Azul"," xx.xxx.xxx/xxxx-xx", "Av. Lins de Vasconcelos, 356","01538-900","(11) 3348-4109", null);
-//				CentroColeta centroColeta4 = new CentroColeta(0,"Hospital São Luiz Gonzaga"," xx.xxx.xxx/xxxx-xx", "Rua Michel Ouchana, 94","02276-140","(11) 3466-1000", null);
-//				CentroColeta centroColeta5 = new CentroColeta(0,"Hemocentro Unifesp"," xx.xxx.xxx/xxxx-xx", "Av. Nazaré, 28 - subsolo","04263-200","(11) 2061-2229", null);
+//				CentroColeta centroColeta1 = new CentroColeta(0,"Hemocentro Unifesp"," xx.xxx.xxx/xxxx-xx", "Rua Botucatu, 620","04023062","1155397289", null);
+//				CentroColeta centroColeta2 = new CentroColeta(0,"Banco de Sangue Paulista"," xx.xxx.xxx/xxxx-xx", " Rua Iguatinga, 396","04744040","1156816222", null);
+//				CentroColeta centroColeta3 = new CentroColeta(0,"Hospital Cruz Azul"," xx.xxx.xxx/xxxx-xx", "Av. Lins de Vasconcelos, 356","01538900","(11) 33484109", null);
+//				CentroColeta centroColeta4 = new CentroColeta(0,"Hospital São Luiz Gonzaga"," xx.xxx.xxx/xxxx-xx", "Rua Michel Ouchana, 94","02276140","(11) 34661000", null);
+//				CentroColeta centroColeta5 = new CentroColeta(0,"Hemocentro Unifesp"," xx.xxx.xxx/xxxx-xx", "Av. Nazaré, 28 - subsolo","04263200","1120612229", null);
 //
 //				// Persistindo objetos centroColeta
 //				em.persist(centroColeta1);
@@ -38,11 +38,11 @@ public class TestesCentroColeta {
 //
 //				em.getTransaction().commit();
 //				
-//				System.out.println("Centro Coleta: "+ centroColeta1 +" inserido realizada com sucesso!" );
-//				System.out.println("Centro Coleta: "+ centroColeta2 +" inserido realizada com sucesso!" );
-//				System.out.println("Centro Coleta: "+ centroColeta3 +" inserido realizada com sucesso!" );
-//				System.out.println("Centro Coleta: "+ centroColeta4 +" inserido realizada com sucesso!" );
-//				System.out.println("Centro Coleta: "+ centroColeta5 +" inserido realizada com sucesso!" );
+//				System.out.println("Centro Coleta: "+ centroColeta1 +" inserido com sucesso!" );
+//				System.out.println("Centro Coleta: "+ centroColeta2 +" inserido com sucesso!" );
+//				System.out.println("Centro Coleta: "+ centroColeta3 +" inserido com sucesso!" );
+//				System.out.println("Centro Coleta: "+ centroColeta4 +" inserido com sucesso!" );
+//				System.out.println("Centro Coleta: "+ centroColeta5 +" inserido com sucesso!" );
 //				
 //
 //			} catch (Exception e) {
@@ -55,7 +55,7 @@ public class TestesCentroColeta {
 //					em.close();
 //				}
 //			}
-//
+
 			// ====================================--Read--=======================================
 	
 			try {
